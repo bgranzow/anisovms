5e46ac0ab6cb9ea409da87fdcc9530a5e5096cca
Brian Granzow <brian.granzow@gmail.com>
2017-12-18 23:03:01 -0500

    cool

--- ex1.cpp
+++ ex1.cpp
@@ -4,42 +4,66 @@
 
 namespace avms {
 
-static char const* set_names[Omega_h::NSET_TYPES] = {
+static char const* type_name[Omega_h::NSET_TYPES] = {
   "elem set", "side set", "node set" };
 
-static void print_mesh_stats(Omega_h::Mesh* m) {
-  if (! Omega_h::can_print(m)) return;
-  std::cout << "num elems: " << m->nelems() << std::endl;
-  std::cout << "num verts: " << m->nverts() << std::endl;
+struct Disc {
+  Disc(int argc, char** argv);
+  void print();
+  Omega_h::Library lib;
+  Omega_h::Mesh mesh;
+  Omega_h::Assoc assoc;
+  Omega_h::MeshSets sets;
+};
+
+Disc::Disc(int argc, char** argv) :
+  lib( Omega_h::Library(&argc, &argv) ),
+  mesh( Omega_h::gmsh::read(argv[1], lib.world()) ) {
+    Omega_h::update_from_file(&assoc, argv[2]);
+    sets = Omega_h::invert(&mesh, assoc); 
 }
 
-static void print_assoc_stats(
-    Omega_h::Mesh* m,
-    Omega_h::Assoc& a,
-    Omega_h::MeshSets& s) {
-  if (! Omega_h::can_print(m)) return;
-  std::cout << "num elem sets: " << a[Omega_h::ELEM_SET].size() << std::endl;
-  std::cout << "num side sets: " << a[Omega_h::SIDE_SET].size() << std::endl;
-  std::cout << "num node sets: " << a[Omega_h::NODE_SET].size() << std::endl;
+void Disc::print() {
+  if (! Omega_h::can_print(&mesh)) return;
+  std::cout << "num elems: " << mesh.nelems() << std::endl;
+  std::cout << "num verts: " << mesh.nverts() << std::endl;
   for (size_t set_type = 0; set_type < Omega_h::NSET_TYPES; ++set_type) {
-    for (auto& name_pairs : a[set_type]) {
+    std::cout << "num " << type_name[set_type] << " ";
+    std::cout << assoc[set_type].size() << std::endl;
+    for (auto& name_pairs : assoc[set_type]) {
       auto name = name_pairs.first;
-      std::cout << set_names[set_type] << " '" << name << "' ";
-      std::cout << "has size : " << s[set_type][name].size();
+      std::cout << type_name[set_type] << " '" << name << "' ";
+      std::cout << "has size: " << sets[set_type][name].size();
       std::cout << std::endl;
     }
   }
 }
 
+struct Data {
+  Data(char** argv);
+  void print(Disc* d);
+  double kappa;
+  Omega_h::Vector<2> alpha;
+};
+
+Data::Data(char** argv) {
+  kappa = std::stod(argv[3]);
+  alpha[0] = std::stod(argv[4]);
+  alpha[1] = std::stod(argv[5]);
+}
+
+void Data::print(Disc* d) {
+  if (! Omega_h::can_print(&(d->mesh))) return;
+  std::cout << "kappa: " << kappa << std::endl;
+  std::cout << "alpha: [" << alpha[0] << ", " << alpha[1] << "]\n";
+}
+
 }
 
 int main(int argc, char** argv) {
-  auto lib = Omega_h::Library(&argc, &argv);
-  auto mesh = Omega_h::gmsh::read("./msh/lshape/lshape.msh", lib.world());
-  Omega_h::Assoc assoc;
-  Omega_h::update_from_file(&assoc, "./msh/lshape/assoc.txt");
-  auto sets = Omega_h::invert(&mesh, assoc);
-  avms::print_mesh_stats(&mesh);
-  avms::print_assoc_stats(&mesh, assoc, sets);
-  Omega_h::vtk::write_vtu("debug.vtu", &mesh);
+  assert(argc == 6); 
+  avms::Disc disc(argc, argv);
+  avms::Data data(argv);
+  disc.print();
+  data.print(&disc);
 }
